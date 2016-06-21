6bbc1e3946da040c58c87a0b0465f8bd977f863d
asmith9 <obssoftware@protonmail.com>
2016-06-21 13:24:13 -0500

    edit to index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var irc = require("irc");
 
 var config = {
 	server: "irc.rizon.net",
-	channels: ["##pasta"],
+	channels: ["#pasta"],
 	ignoreList: ["CummyPawsBot", "Combot", "PastaBot", "cuckbot", "kekbot", "pepebot", "katbot"],
 	nick: "usernick",
 	userName: "username",
-	realName: "realname",
+	realName: "man",
 	nickservEnabled: true,
 	nickservPass: "userpass"
 }
@@ -24,16 +24,22 @@ bot.addListener("message", function(from, to, message) {
 	message.split(" ").forEach(function(element){
 		word = element.replace(/[^a-zA-Z]+/g, '')
 		
-		if (message.indexOf('DRAGON')>-1
-		   || message.indexOf('DRAGONS')>-1
-		   || message.indexOf('DRAG-ONS')>-1
-		   || message.indexOf('..dragons')>-1
+		if (message.indexOf('spook')>-1
 		   ){man=message;}
 	
 	});
-//	pause(3000);
-	bot.say(to, message)
-
+	pause(3000);
+	bot.say(to, '5')
+	pause(3000);
+	bot.say(to, '4')
+	pause(3000);
+	bot.say(to, '3')
+	pause(3000);
+	bot.say(to, '2')
+	pause(3000);
+	bot.say(to, '1')
+	pause(3000);
+	bot.say(to, "boo! ^.~")
 });
 
 bot.addListener("ctcp-version", function(from, to, message) {
@@ -54,3 +60,4 @@ bot.on("invite", function(channel, nick) {
 	bot.join(channel)
 	return bot.say(channel, nick + " has invited me here, enjoy man!")
 })
+
