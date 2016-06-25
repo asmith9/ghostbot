f7d519ad9c6645e988e87194786a3a3c9890237f
asmith9 <obssoftware@protonmail.com>
2016-06-25 14:30:01 -0500

    added modules

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
 var irc = require("irc"); 
+var math = require("math");
 
 var config = {
 	server: "irc.rizon.net",
-	channels: ["##pasta"],
+	channels: ["#pasta"],
 	ignoreList: ["CummyPawsBot", "Combot", "PastaBot", "cuckbot", "kekbot", "pepebot", "katbot"],
 	nick: "ghost_bot",
 	userName: "username",
@@ -12,23 +12,35 @@ var config = {
 	nickservPass: "userpass"
 }
 var bot = new irc.Client(config.server, config.nick, config)
+var tombstone= [" |________|",
+		" |  RIP   |",
+		"  /￣￣￣\\"]
+var faces = ["(ц｀ω´ц*)","“ψ(｀∇´)ψ","ψ(*｀ー´)ψ","Ψ(｀▽´)Ψ",
+	    "Ψ(｀◇´)Ψ","(屮｀∀´)屮","Щ(･｀ω´･Щ)","Ψ(￣∀￣)Ψ",
+	    "Ψ(☆ｗ☆)Ψ","Ψ( ●｀▽´● )Ψ","ψ（｀Д´）ψ","ლ(｀∀´ლ)",
+	    "＜(●｀∀´●)＞”","o(○｀ω´○)9","ρ(｀.´)ρ","पुनः कदा मेलिष्यामः ?"]
+var randomnumber = 1;
+
 
 bot.addListener("message", function(from, to, message) {
 	if (config.ignoreList.indexOf(from) >= 0){ return false }
 		var man
 	message.split(" ").forEach(function(element){
 		word = element.replace(/[^a-zA-Z]+/g, '')
-		if (word.toLowerCase() == "spook") {
+		if (word.toLowerCase() == "rip") {
 	function loop(n) {
-		if (n>0) {
+		if (n>=0) {
 		   setTimeout(function()
-		   { bot.say(to,n); loop(n -1 );}, 3000);
-		   } else {
-			   bot.say(to,"boo! ^.~");
-		   }
+		   { bot.say(to,tombstone[n]); loop(n-1);}, 1000);
+		   }  
+	        }
+        	loop(4);
 		}
-		loop(5);
-		}
+		else if (word.toLowerCase() == "spooky"
+			||word.toLowerCase()=="scary"
+		        ||word.toLowerCase()=="skeletons") {
+			randomnumber=math.floor(Math.random()*16)
+			bot.say(to, faces[randomnumber]);}
 	});
 });
 
